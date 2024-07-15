d096f127d52f4be991e25fd61b69ac396c32a1cd
jcraiz <63567373+jcraiz@users.noreply.github.com>
2024-07-15 00:54:39 -0500

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,18 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "65cMfSwf4ry":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
---
+++ story_content/user.js
@@ -0,0 +1,36 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  var player = GetPlayer();
+
+$.ajax({
+    url: "https://script.google.com/macros/s/AKfycbyxH5uQhEACmohD4wxCxjEzlvUpRfsPYvySKMWZJ6IhlRnesRU4nfz1RtD0jvb3ZUOtRw/exec",
+    type: "POST",
+    data: {
+        "IdNumber": player.GetVar("IdNumber"),
+        "FullName": player.GetVar("FullName"),
+        "Age": player.GetVar("Age"),
+        "PuntajePerfil": player.GetVar("PuntajePerfil"),
+        
+    },
+    success: function(data) {
+        if (data.Timestamp) {
+            console.log("Timestamp:", data.Timestamp);
+        }
+        console.log(data);
+    },
+    error: function(err) {
+        console.log('Error:', err);
+    }
+});
+
+return false;
+}
+
+};
