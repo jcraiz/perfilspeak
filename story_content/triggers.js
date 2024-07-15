81f8c51cfc3413e9cd1d1f3bbad42c224fc2d73e
jcraiz <63567373+jcraiz@users.noreply.github.com>
2024-07-15 01:58:02 -0500

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,24 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6oQ6VWkpXdJ":
+        Script1();
+        break;
+      case "5XJWsmGXz9i":
+        Script2();
+        break;
+      case "6ITQehdkeTM":
+        Script3();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
---
+++ story_content/user.js
@@ -0,0 +1,104 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  var player = GetPlayer();
+
+$.ajax({
+    url: "https://script.google.com/macros/s/AKfycbwPpHxpTcDNTgV-BMSP8rxFKFlhP1KRAn3EJoP2hemYNhlAqtV2GGmhYRNcY3TAPqY6/exec",
+    type: "POST",
+    data: {
+        "date" : new Date().toJSON().slice(0,10), 
+        "IdNumber": player.GetVar("IdNumber"),
+        "FullName": player.GetVar("FullName"),
+        "Age": player.GetVar("Age"),
+        "PuntajePerfil": player.GetVar("PuntajePerfil"),
+        
+    },
+    success: function(data)
+
+{
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
+}
+
+});
+
+return false;
+}
+
+window.Script2 = function()
+{
+  var player = GetPlayer();
+
+$.ajax({
+    url: "https://script.google.com/macros/s/AKfycbwPpHxpTcDNTgV-BMSP8rxFKFlhP1KRAn3EJoP2hemYNhlAqtV2GGmhYRNcY3TAPqY6/exec",
+    type: "POST",
+    data: {
+        "date" : new Date().toJSON().slice(0,10), 
+        "IdNumber": player.GetVar("IdNumber"),
+        "FullName": player.GetVar("FullName"),
+        "Age": player.GetVar("Age"),
+        "PuntajePerfil": player.GetVar("PuntajePerfil"),
+        
+    },
+    success: function(data)
+
+{
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
+}
+
+});
+
+return false;
+}
+
+window.Script3 = function()
+{
+  var player = GetPlayer();
+
+$.ajax({
+    url: "https://script.google.com/macros/s/AKfycbwPpHxpTcDNTgV-BMSP8rxFKFlhP1KRAn3EJoP2hemYNhlAqtV2GGmhYRNcY3TAPqY6/exec",
+    type: "POST",
+    data: {
+        "date" : new Date().toJSON().slice(0,10), 
+        "IdNumber": player.GetVar("IdNumber"),
+        "FullName": player.GetVar("FullName"),
+        "Age": player.GetVar("Age"),
+        "PuntajePerfil": player.GetVar("PuntajePerfil"),
+        
+    },
+    success: function(data)
+
+{
+
+console.log(data);
+
+},
+
+error: function(err) {
+console.log('Error:', err);
+}
+
+});
+
+return false;
+}
+
+};
